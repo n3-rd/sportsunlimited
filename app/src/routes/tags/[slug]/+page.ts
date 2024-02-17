eb9af1025ca012af5db1a48151153b48d2b794cd
n3-rd <godwinjdayo@gmail.com>
2024-02-17 11:51:46 +0100

    fixed slug capitalisation for tags

--- app/src/routes/tags/[slug]/+page.ts
+++ app/src/routes/tags/[slug]/+page.ts
@@ -3,7 +3,10 @@ import { getTaggedPosts } from '$lib/utils/sanity';
 export const ssr = false;
 
 export async function load({ params}) {
+    const capitalizeFirstLetter = (string:string) =>{
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
     const { slug } = params;
-    const posts = await getTaggedPosts(slug);
+    const posts = await getTaggedPosts(capitalizeFirstLetter(slug));
     return { props: { posts, slug } };
 }
