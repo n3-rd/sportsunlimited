ec904a5bdf545e9dcd7d20c86bb9f5c9b194c083
n3-rd <godwinjdayo@gmail.com>
2024-02-23 15:33:38 +0100

    fix: vercel analytics

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
 	import Footer from '../components/Footer.svelte';
 	import Header from '../components/Header.svelte';
+	// Import the Analytics package, and the SvelteKit dev variable.
+	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+
+	// Inject the Analytics functionality
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Header />
