8dfa24fb57f0cc4314535d399152e3d189113d2f
n3-rd <godwinjdayo@gmail.com>
2025-09-14 22:55:34 +0100

    removed yanga ad

--- app/src/components/Sidebar.svelte
+++ app/src/components/Sidebar.svelte
@@ -1,4 +1,4 @@
 <div class="flex flex-col h-full w-full gap-3">
-    <a href="/yanga.pdf"><img src="/yanga-ad.png" alt="ad placement"></a>
+    <!-- <a href="/yanga.pdf"><img src="/yanga-ad.png" alt="ad placement"></a> -->
     <img src="/book.jpg" alt="eyes on the nigerin league book">
 </div>
